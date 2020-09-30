31d0a8b2b0021eb5d44dda7fa04f80b8fa0d3b2e
akhare23 <70730778+akhare23@users.noreply.github.com>
2020-09-29 21:56:01 -0700

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,25 @@
+class Box{
+    constructor(x,y,width,height){
+
+        var options = {
+            isStatic:true
+        }
+
+        
+
+        this.body = Bodies.rectangle(x,y,width,height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body);
+    }
+
+    display(){
+        var pos = this.body.position
+
+        fill("brown");
+        stroke("blue");
+        strokeWeight(4);
+        rectMode(CENTER);
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+}
